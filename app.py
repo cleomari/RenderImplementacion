0bf2a9e36398da0402b2d4334f1fdd05f47877ff
cleomari <al062090@uacam.mx>
2025-05-03 19:25:45 -0600

    Versión inicial de mi app en Heroku

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, redirect
+import psycopg2
+import os
+
+app = Flask(__name__)
+
+def get_db_connection():
+    conn = psycopg2.connect(os.environ['DATABASE_URL'], sslmode='require')
+    return conn
+
+@app.route('/')
+def index():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('CREATE TABLE IF NOT EXISTS tareas (id SERIAL PRIMARY KEY, texto TEXT);')
+    cur.execute('SELECT * FROM tareas;')
+    tareas = cur.fetchall()
+    conn.close()
+    return render_template('index.html', tareas=tareas)
+
+@app.route('/add', methods=['POST'])
+def add():
+    texto = request.form['texto']
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('INSERT INTO tareas (texto) VALUES (%s);', (texto,))
+    conn.commit()
+    conn.close()
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run()
